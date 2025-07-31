b9f880795a02400a81f55e33330a9adeda0bd76e
Colin Beeby <colin.beeby1@nhs.net>
2025-07-31 13:29:00 +0100

    Parameterized terraform configuration

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "ResourceGroupName" {
+    type = string
+}
+
+variable "ResourceGroupLocation" {
+    type = string
+}
+
+variable "AppServicePlanName" {
+    type = string
+}
+
+variable "AppServicePlanKind" {
+    type = string
+}
+
+variable "AppServicePlanSkuTier" {
+    type = string
+}
+
+variable "AppServicePlanSkuSize" {
+    type = string
+}
+
+variable "AppServiceName" {
+    type = string
+}
